5077875eeabaf07ad04ff20fc591a62385d102c9
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2023-01-02 09:49:09 -0300

    build: maven project

---
+++ src/test/java/com/algalog/AlgalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
