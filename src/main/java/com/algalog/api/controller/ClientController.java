0daaa03071fabdbd0f3d1ccc5849a9cc25aa7431
Rodrigo <rodrigoml2395@gmail.com>
2023-01-21 21:15:37 -0300

    feat: find by id

--- src/main/java/com/algalog/api/controller/ClientController.java
+++ src/main/java/com/algalog/api/controller/ClientController.java
@@ -1,22 +1,35 @@
 package com.algalog.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algalog.domain.model.Client;
 import com.algalog.domain.repository.ClientRepository;
 
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 	
 	@Autowired
 	private ClientRepository clientRespository;
 	
-	@GetMapping("/clients")
+	@GetMapping
 	public List<Client> list() {
 		return clientRespository.findAll();
+	}
+	
+	@GetMapping("/{clientId}")
+	public ResponseEntity<Client> search(@PathVariable Long clientId) {
+		return clientRespository.findById(clientId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+		
 	}
 }
