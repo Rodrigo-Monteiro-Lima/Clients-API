d48806ffccbb71aaf137f27635c5c31be08364ad
Rodrigo <rodrigoml2395@gmail.com>
2023-01-21 21:30:10 -0300

    feat: update client

--- src/main/java/com/algalog/api/controller/ClientController.java
+++ src/main/java/com/algalog/api/controller/ClientController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -40,5 +41,17 @@ public class ClientController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Client add(@RequestBody Client client) {
 		return clientRespository.save(client);
+	}
+	
+	@PutMapping("/{clientId}")
+	public ResponseEntity<Client> update(@PathVariable Long clientId, @RequestBody Client client) {
+		if (!clientRespository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		client.setId(clientId);
+		client = clientRespository.save(client);
+		
+		return ResponseEntity.ok(client);
 	}
 }
