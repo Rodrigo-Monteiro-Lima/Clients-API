6824d217bd1bd33dace2add08bc376f2412532b8
Rodrigo <rodrigoml2395@gmail.com>
2023-01-29 07:17:07 -0300

    feat: client id input

---
+++ src/main/java/com/algalog/api/model/input/ClientIdInput.java
@@ -0,0 +1,15 @@
+package com.algalog.api.model.input;
+
+public class ClientIdInput {
+	
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
