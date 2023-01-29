aa2fdb4668cdde13af6be142eea32e8780e46f37
Rodrigo <rodrigoml2395@gmail.com>
2023-01-28 22:58:32 -0300

    feat: recipient model

---
+++ src/main/java/com/algalog/api/model/RecipientModel.java
@@ -0,0 +1,41 @@
+package com.algalog.api.model;
+
+public class RecipientModel {
+	
+	private String name;
+	private String adress;
+	private String number;
+	private String complement;
+	private String neighborhood;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getComplement() {
+		return complement;
+	}
+	public void setComplement(String complement) {
+		this.complement = complement;
+	}
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+}
