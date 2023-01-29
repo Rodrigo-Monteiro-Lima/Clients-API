15a8c80952964d87c235808d4075dce9d42ff578
Rodrigo <rodrigoml2395@gmail.com>
2023-01-29 07:24:02 -0300

    feat: delivery input

--- src/main/java/com/algalog/api/model/input/DeliveryInput.java
+++ src/main/java/com/algalog/api/model/input/DeliveryInput.java
@@ -1,5 +1,7 @@
 package com.algalog.api.model.input;
 
+import java.math.BigDecimal;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
@@ -11,4 +13,25 @@ public class DeliveryInput {
 	@Valid
 	@NotNull
 	private RecipientInput recipient;
+	@NotNull
+	private BigDecimal fee;
+	
+	public ClientIdInput getClient() {
+		return client;
+	}
+	public void setClient(ClientIdInput client) {
+		this.client = client;
+	}
+	public RecipientInput getRecipient() {
+		return recipient;
+	}
+	public void setRecipient(RecipientInput recipient) {
+		this.recipient = recipient;
+	}
+	public BigDecimal getFee() {
+		return fee;
+	}
+	public void setFee(BigDecimal fee) {
+		this.fee = fee;
+	}
 }
